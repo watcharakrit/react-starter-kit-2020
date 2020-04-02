314824f0dd098c46f4495cdc050d8482991066c0
Watcharakrit Phantu <watcharakrit@gmail.com>
2020-04-03 06:38:55 +0700

    Setup create react app with overrides config.

---
+++ README.md
@@ -0,0 +1,14 @@
+SeeDoctorNow Mobile Client ( with Create React App ).
+
+## Prerequisites
+- Node.js ( 10.16.3 )
+- Yarn
+
+## Development
+```
+yarn start
+```
+
+Resources for developers:
+
+- &rarr; This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ config-overrides.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const {
+  override,
+  addWebpackAlias,
+} = require('customize-cra')
+// const rewireStyledComponents = require('react-app-rewire-styled-components')
+
+module.exports = override(
+  addWebpackAlias({
+    '<Layouts>': path.resolve(__dirname, 'src/layouts'),
+    '<Screens>': path.resolve(__dirname, 'src/screens'),
+    '<Styles>': path.resolve(__dirname, 'src/styles'),
+    '<UI>': path.resolve(__dirname, 'src/shared/base-ui'),
+    '<Features>': path.resolve(__dirname, 'src/shared/features'),
+    '<FeaturesMultiStore>': path.resolve(__dirname, 'src/shared/features-multi-store'),
+    '<Helpers>': path.resolve(__dirname, 'src/utils'),
+    '<State>': path.resolve(__dirname, 'src/state'),
+    '<Hooks>': path.resolve(__dirname, 'src/shared/hooks'),
+    '<Shared>': path.resolve(__dirname, 'src/shared'),
+  }),
+  // (config, env) => {
+  //   config = rewireStyledComponents(config, env, {
+  //     displayName: true,
+  //   })
+  //   return config
+  // },
+)
